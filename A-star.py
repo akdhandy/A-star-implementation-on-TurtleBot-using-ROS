15959b130c261ff27d7c1fc229fd4166710af424
aarjunsrinivasan <arjunsrinivasan.a@gmail.com>
2020-03-20 15:39:33 -0400

    A-star WIP

--- A-star.py
+++ A-star.py
@@ -1,40 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 18 19:25:54 2020
-
-@author: arjun
-"""
-
-#!/usr/bin/env python
-# coding: utf-8
-"""
-Created on Fri Mar  6 18:54:30 2020
-
-@author: Arun
-"""
 import math
 import numpy as np
 import cv2 as cv
 import time
 from scipy.spatial import distance
-from math import pi,sin,atan
+from math import pi,sin,atan,cos
 
 
-
-rad=int(input('Please enter radius of the robot:'))
-clearance=int(input('Please enter the clearance:'))
-
-tot=rad+clearance
-
-
+#############  BOUNDS FUNCTION ####################################
 #Checking the boundary conditions for the obstacle space
 def boundary_check(i,j):
     if (i<tot or j>199-tot or j<tot or i>299-tot):
         return 0
     else:
         return 1
-
 #original obs
 def obs_map_pt(x,y):
     circle = ((np.square(x-225))+ (np.square(y-50)) <=np.square(25))
@@ -51,7 +29,6 @@ def obs_map_pt(x,y):
         obj_val = 1
     
     return obj_val
-
 #Obstacle Map with Rigid body clearence
 def obs_map(x,y):
     circle = ((np.square(x-225))+ (np.square(y-50)) <=np.square(25+tot))
@@ -68,39 +45,19 @@ def obs_map(x,y):
     
     return obj_val
 
-x = 300
-y = 200
-image = np.ones((y,x,3),np.uint8)*255
+###############  TAKING INPUTS ################################
+rad=int(input('Please enter radius of the robot:'))
+clearance=int(input('Please enter the clearance:'))
 
-for i in range(200):
-    for j in range(300):
-            image[i][j] = (175,175,175)
-            
-#Outer edges mapping
-for x in range(300):
-    for y in range(200):
-        if obs_map(x,y)==0:
-                 image[y,x] = 0
-                 
-                 #Outer edges mapping
-for x in range(300):
-    for y in range(200):
-        if obs_map_pt(x,y)==0:
-                 image[y,x] = (255,255,255)
-
-
-parent_list=[]
-for j in range (300):
-    column=[]
-    for i in range (200):
-        column.append(0)
-    parent_list.append(column)
+tot=rad+clearance
 
 #Step size
 s=int(input("Please enter the step size from 1 to 10:"))
 #Getting the start nodes from the user
 x_start = int(input("Please enter start point x coordinate:"))
 y_start = int(input("Please enter start point y coordinate:"))
+theta_start = int(input("Please enter start orientation in degrees:"))
+
 y_start = 199 - y_start
 
 start_obs = obs_map(x_start,y_start)
@@ -110,7 +67,9 @@ while(start_obs!=1 or start_boundary!=1):
     print("Incorrect start point! Please enter a valid start point:")
     x_start = int(input("Please enter start point x coordinate:"))
     y_start = int(input("Please enter start point y coordinate:"))
+    theta_goal = int(input("Please enter goal orientation in degrees:"))
     y_start = 199 - y_start
+    theta_start = int(input("Please enter start orientation in degrees:"))
     start_obs = obs_map(x_start,y_start)
     start_boundary = boundary_check(x_start,y_start)
  
@@ -122,6 +81,8 @@ start=[x_start,y_start]
 x_goal=int(input("Please enter goal point x coordinate:"))
 y_goal=int(input("Please enter goal point y coordinate:"))
 y_goal=199 - y_goal
+theta_goal = int(input("Please enter goal orientation in degrees:"))
+
 
 goal_obs=obs_map(x_goal,y_goal)
 goal_boundary=boundary_check(x_goal,y_goal)
@@ -131,6 +92,8 @@ while(goal_obs!=1 or goal_boundary!=1):
     print("Incorrect goal point! Please enter a valid goal point:")
     x_goal=int(input("Please enter another goal point x coordinate:"))
     y_goal=int(input("Please enter another goal point y coordinate:"))
+    theta_goal = int(input("Please enter goal orientation in degrees:"))
+
     y_goal=199 - y_goal
     goal_obs=obs_map(x_goal,y_goal)
     goal_boundary=boundary_check(x_goal,y_goal)
@@ -139,54 +102,43 @@ while(goal_obs!=1 or goal_boundary!=1):
 goal=[x_goal,y_goal]
 
 
-
+###################   INTIALISING COST,VISITED,PARENT AND QUEUE  ###############
 #Initializing cost as infinity 
-cost_array=np.array(np.ones((300,200)) * np.inf)
+cost_array=np.array(np.ones((600,400,12)) * np.inf)
 #Initializing visited nodes as empty array
 visited=np.array(np.zeros((600,400,12)))
 #Heuristic distance-Eucledian
-euclidean_array=np.array(np.ones((300,200)) * np.inf)
+euclidean_array=np.array(np.ones((600,400,12)) * np.inf)
 #Initializing total cost with cost and distance
-totalcost=np.array(np.ones((300,200)) * np.inf)
-
-#Size of workspace
-size=(200,300)
-
-mapy=np.ones((size),np.uint8)*255
-
-
+totalcost=np.array(np.ones((600,400,12)) * np.inf)
+parent_list=np.zeros((600,400,12))
+parent_list=list(parent_list)
 Q=[]
 
 # append start point and initialize it's cost to zero
+Q.append([x_start,y_start,theta_start])
+cost_array[x_start][y_start][theta_start]=0
+totalcost[x_start][y_start][theta_start]=0
 
-Q.append([x_start,y_start])
-cost_array[x_start][y_start]=0
-totalcost[x_start][y_start]=0
-print(Q)
 # Priority Queue Function
-
 def pop(Q):
     minimum_index=0
     minimum_X = Q[0][0] 
     minimum_Y = Q[0][1]
+    minimum_th=Q[0][2]
     for i in range(len(Q)):
         x = Q[i][0]
         y = Q[i][1]
-        if cost_array[x,y] < cost_array[minimum_X,minimum_Y]:
+        z=Q[i][2]
+        if totalcost[x,y,z] < totalcost[minimum_X,minimum_Y,minimum_th]:
             minimum_index = i
             minimum_X = x 
             minimum_Y= y
+            minimum_th=z
         
     current_node = Q[minimum_index]
     Q.remove(Q[minimum_index])
     return current_node
-
-
-
-# # All possible movements
-# def north(i,j):
-#     new_node=[i,j+1]     
-#     return new_node
 
 
 def move(i,j,t,s):
@@ -196,36 +148,31 @@ def move(i,j,t,s):
 
 #Djikstra Algorithm
 start_time=time.time()
-goalpoint=[x_goal,y_goal]    
+goalpoint=[x_goal,y_goal,theta_goal]    
 visited_node=[]
-current_node=[x_start,y_start]
-while current_node!=goal:
+current_node=[x_start,y_start,theta_start]
+while current_node[0:2]!=goal[0:2]:
     current_node=pop(Q)
     
     for t in range(0,360,30):
-        new_node=move(current_node[0],current_node[1],t,s)
-    # new_north=north(current_node[0],current_node[1])
-    # status=boundary_check(new_north[0],new_north[1])
-    # flag=obs_map(new_north[0],new_north[1])
-    # if (status and flag == 1):
-    #     if visited[new_north[0],new_north[1]]==0:
-    #         visited[new_north[0],new_north[1]]=1
-    #         visited_node.append(new_north)
-    #         Q.append(new_north)
-    #         parent_list[new_north[0]][new_north[1]]=current_node
-    #         cost_array[new_north[0],new_north[1]]=(cost_array[current_node[0],current_node[1]]+1)
-    #         euclidean_array[new_north[0],new_north[1]]=distance.euclidean(new_north, goalpoint)
-    #         totalcost[new_north[0],new_north[1]]= cost_array[new_north[0],new_north[1]]+euclidean_array[new_north[0],new_north[1]]
-    #     else:
-    #         if cost_array[new_north[0],new_north[1]]>(cost_array[current_node[0],current_node[1]]+1):
-    #             cost_array[new_north[0],new_north[1]]=(cost_array[current_node[0],current_node[1]]+1)
-    #             parent_list[new_north[0]][new_north[1]]=current_node
-    #             euclidean_array[new_north[0],new_north[1]]=distance.euclidean(new_north, goalpoint)
-    #             totalcost[new_north[0],new_north[1]]= cost_array[new_north[0],new_north[1]]+euclidean_array[new_north[0],new_north[1]]
+        curr_node=move(current_node[0],current_node[1],t,s)
+        status=boundary_check(curr_node[0],curr_node[1])
+        flag=obs_map(curr_node[0],curr_node[1])
+        if (status and flag == 1):
+          if visited[2*curr_node[0],2*curr_node[1]][t]==0:
+            visited[curr_node[0],curr_node[1]]=1
+            visited_node.append(curr_node)
+            Q.append(curr_node)
+            parent_list[curr_node[0]][curr_node[1]]=current_node
+            cost_array[curr_node[0],curr_node[1]]=(cost_array[current_node[0],current_node[1]]+s)
+            euclidean_array[curr_node[0],curr_node[1]]=distance.euclidean(curr_node, goalpoint)
+            totalcost[curr_node[0],curr_node[1]]= cost_array[curr_node[0],curr_node[1]]+euclidean_array[curr_node[0],curr_node[1]]
+          else:
+            if totalcost[curr_node[0],curr_node[1]]>(totalcost[current_node[0],current_node[1]]+1):
+                totalcost[curr_node[0],curr_node[1]]=(totalcost[current_node[0],current_node[1]]+1)
+                parent_list[curr_node[0]][curr_node[1]]=current_node
   
 print("Goal reached")
-
-
 
 #Pathfinder function
 goal=[x_goal,y_goal]
@@ -240,38 +187,30 @@ def path_find(goal,start):
         GN=a
 
 path_find(goal,start)
-
-
-
-print('The cost of the shortest path is',cost_array[x_goal,y_goal])
-
-pic=cv.resize(image,None,fx=2,fy=2)
-
-
-#Showing the graphical output
-cv.circle(image,(int(goal[0]),int(goal[1])), (1), (0,0,255), -1);
-cv.circle(image,(int(start[0]),int(start[1])), (1), (0,0,255), -1);
-
-for i in visited_node:
-    cv.circle(image,(int(i[0]),int(i[1])), (1), (255,0,0));
-    pic=cv.resize(image,None,fx=2,fy=2)
-    cv.imshow('map',pic)
-    cv.waitKey(1)
-
-for i in path:
-    cv.circle(image,(int(i[0]),int(i[1])), (1), (150,50,204));
-    pic=cv.resize(image,None,fx=2,fy=2)
-    cv.imshow('map',pic)
-    cv.waitKey(1)
-    
-print("Total time:")
-print(time.time()-start_time)   
-cv.waitKey(0) 
-cv.destroyAllWindows()
+print('The cost of the shortest path is',totalcost[x_goal,y_goal])
 
 
 
 
+
+
+
+#############   PLOTTING STARTS ####################
+plot_x=[]
+plot_y=[]
+
+for i in range(200):
+    for j in range(300):
+         if( obs_map(j, i)==0) :
+            plot_x.append(j)
+            plot_y.append(i)
+plt.figure(figsize=(9,6))
+plt.xlim(0,300)
+plt.ylim(0,200)
+plt.plot(plot_x,plot_y,".r") 
+plt.show() 
+plt.pause(5) 
+plt.close()
 
 
 
